ba8e922047d8fef1d989a327c57b5a70182ed102
liviaAugusta <liviamattoni@gmail.com>
2022-05-19 22:18:15 +0200

    correz. es palindromia

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -1,26 +1,30 @@
 // Palindroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+const textUser = document.getElementById('parola-utente');
+const button = document.getElementById('invio');
+const risultato = document.getElementById('risultato');
 
+function palindroma(parola){
 
-function parolaPalindromia(parola){
     let parolaUtenteInversa = '';
+
     for (let i = 1; i <= parola.length; i++){
         parolaUtenteInversa += parola[parola.length - i]
+
     } if (parola === parolaUtenteInversa) {
         return "La parola é palindroma";
+
     } else {
         return "La parola non é palindroma";
     }   
 }
 
-
-const textUser = document.getElementById('parola-utente');
-const button = document.getElementById('invio');
-const risultato = document.getElementById('risultato');
 button.addEventListener('click',
+
       function () {
-          let palindromia = palindromia(textUser.value);
+          let palindromia = palindroma(textUser.value);
           risultato.innerText = palindromia;
       }
+      
 );
